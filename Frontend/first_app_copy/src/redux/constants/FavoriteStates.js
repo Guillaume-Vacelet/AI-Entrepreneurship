a4c34fc273082f5433fce207fc8a42de32bed745
guillaume-vacelet <guillaume.vacelet@epitech.eu>
2021-04-04 12:30:19 +0200

    :fire: Removed useless copy of first_app.

--- Frontend/first_app_copy/src/redux/constants/FavoriteStates.js
+++
@@ -1,2 +0,0 @@
-export const FAV = 'FAV'
-export const NOT_FAV = 'NOT_FAV'
--- Frontend/first_app_copy/src/redux/reducers/reducers-index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux'
-
-import favoriteToggler from './favoriteToggler'
-
-const rootReducer = combineReducers({
-  favoriteToggler
-})
-
-export default rootReducer
