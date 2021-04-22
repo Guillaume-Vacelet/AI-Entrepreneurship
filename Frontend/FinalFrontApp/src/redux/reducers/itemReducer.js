5c9945bdafbefc339bbc1f19b3984175d3794795
guillaume-vacelet <guillaume.vacelet@epitech.eu>
2021-04-22 17:41:13 +0200

    :fire: Removed useless debug line.

--- Frontend/FinalFrontApp/src/redux/reducers/itemReducer.js
+++ Frontend/FinalFrontApp/src/redux/reducers/itemReducer.js
@@ -40,7 +40,6 @@ const initialState = {
 export default function(state = initialState, action) {
 	switch (action.type) {
 		case ADD_ITEM: {
-			console.log(state)
 			return {
 				...state,
 				items: [...state.items, action.payload]
